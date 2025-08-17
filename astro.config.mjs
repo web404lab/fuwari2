b7964d452b832774ee4a89ec3078de607f34bf50
web404lab <101460739+web404lab@users.noreply.github.com>
2025-08-17 12:20:07 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://web404lab.github.io/",
-	base: "/fuwari2",
+	site: "https://myblog.999666666.xyz/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
